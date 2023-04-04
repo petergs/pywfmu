8c74f1b97ad5e8c66de5a8189adb0da8f9dcdf82
Peter Stratis <contact@peter.gs>
2023-04-04 00:07:19 -0400

    Add favorites, update README.md
    This commit updates README.md with a quick usage example. It also
    adds some functionality to favorite songs based on a playlist_id
    and song_id. Currently lacks some error handling.

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # pywfmu
 > A python library for interacting with wfmu.org
 
+This is still very much a WIP.
+
+## Quickstart
+Get the current live show and song
+```
+>>> from pywfmu import WFMUClient
+>>> woofmoo = WFMUClient()
+>>> woofmoo.update_status() 
+>>> woofmoo.show
+{'name': "Daniel Blumin's show", 'playlist_id': '126345', 'playlist_link': 'https://www.wfmu.org/playlists/shows/126345'}
+>>> woofmoo.song
+{'title': 'Kizmiaz', 'artist': 'Don & Francoiz', 'album': 'Cover Songs in Inferno'}
+```
+
 ## Acknowledgments
 - WFMU.org is powered by KenzoDB, by Ken Garson (http://kenzodb.com).
 - Thanks to [BurpSuite](https://portswigger.net/burp) and [Charles](https://www.charlesproxy.com/) for making this project much easier.
--- src/pywfmu/wfmu.py
+++ src/pywfmu/wfmu.py
@@ -9,33 +9,70 @@ from bs4 import BeautifulSoup
 
 
 STATUS_JSON_URL = "https://wfmu.org/wp-content/themes/wfmu-theme/status/main.json"
+CURRENT_SHOW_JSON_URL = "https://wfmu.org/currentliveshows.php?json=1"
 CAMPAIGN_JSON_URL = "https://pledge.wfmu.org/static/progress/campaign.json"
 PLAYLIST_URL_BASE = "https://www.wfmu.org/playlists/shows/"
-COMMENTS_XML_URL = "http://wfmu.org/current_playlist_xml.php?m=comments&c=1"
+COMMENTS_XML_URL = "https://wfmu.org/current_playlist_xml.php?m=comments&c=1"
+CURRENT_SHOW_XML_URL = "https://wfmu.org/currentliveshows.php?xml=1&c=1"
 
 
 class WFMUClient(object):
-    def __init__(
-        self,
-        cookies_file="cookies.txt",
-    ):
+    def __init__(self):
         self.session = requests.Session()
-        # jar = http.cookiejar.FileCookieJar(cookies_file)
-        # self.session.cookies = jar
-        self.update_status()
+        self._update_status()
+        self.key = None
 
     @property
     def artist(self):
+        self._update_status()
         return self.song["artist"]
 
     @property
     def title(self):
-        return self.title["title"]
+        self._update_status()
+        return self.song["title"]
 
     @property
     def album(self):
+        self._update_status()
         return self.song["album"]
 
+    @property
+    def playlist_id(self):
+        self._update_status()
+        return self.show["playlist_id"]
+
+    @property
+    def example(self):
+        self._update_status()
+        return self._example
+
+    def _update_status(self) -> None:
+        r = requests.get(url=CURRENT_SHOW_JSON_URL)
+        status = r.json()
+
+        self.show = {
+            "name": status["program"]["title_html"],
+            "playlist_id": status["episode"]["id"],
+            "playlist_link": status["episode"]["url"],
+            "show_id": status["program"]["id"],
+            "start": status["program"]["start_time_mmss"],
+            "end": status["program"]["end_time_mmss"],
+            "live": status["episode"]["live_indicator_flag"],
+            "setbreak": status["segment"]["set_break_flag"],
+        }
+        self.song = {
+            "title": status["segment"]["title_html"],
+            "artist": status["segment"]["artist_html"],
+            "album": status["segment"]["album_html"],
+            "year": status["segment"]["year_html"],
+            "record_label": status["segment"]["record_label_html"],
+            "song_id": status["segment"]["song_fav_id"],
+        }
+
+        self._example = {"first": 1, "second": 2}
+
+    # session and login
     def login(self, username: str, password: str) -> None:
         self.username = username
         payload = {
@@ -44,14 +81,9 @@ class WFMUClient(object):
         }
         r0 = self.session.get("https://wfmu.org/auth.php", params=payload)
 
-        soup = BeautifulSoup(r0.text, features="lxml")
-        inputs = soup.find_all("input")
-        for inp in inputs:
-            if inp.get("name") == "__kfid":
-                kfid = inp.get("value")
-
+        vals = self._extract_input_values(["__kfid"], r0.text)
         body = {
-            "__kfid": kfid,
+            "__kfid": vals["__kfid"],
             "a": "login_submit",
             "r": "https://wfmu.org/index.shtml",
             "sk": "",
@@ -60,28 +92,10 @@ class WFMUClient(object):
             "login": "Sign in",
         }
         r1 = self.session.post("https://wfmu.org/auth.php", params=payload, data=body)
+        self.key = vals["__kfid"]
+        print(self.key)
 
-    def update_status(self) -> None:
-        r = requests.get(url=STATUS_JSON_URL)
-        status = r.json()
-
-        self.show = {
-            "name": status["show"],
-            "playlist_id": status["playlist"]["@attributes"]["id"],
-            "playlist_link": f"{PLAYLIST_URL_BASE}{status['playlist']['@attributes']['id']}",
-        }
-        self.song = {
-            "title": status["title"],
-            "artist": status["artist"],
-            "album": status["album"],
-        }
-        self.live = (
-            True if status["liveIndicator"]["@attributes"]["flag"] == "1" else False
-        )
-        self.setbreak = (
-            True if status["setBreak"]["@attributes"]["flag"] == "1" else False
-        )
-
+    # comments
     def comment(self, comment: str) -> None:
         r0 = self.session.get(self.show["playlist_link"])
         e = self._extract_input_values(["e"], r0.text)
@@ -96,6 +110,8 @@ class WFMUClient(object):
         r1 = self.session.post("https://wfmu.org/playlistcommentpost.php", data=body)
 
         vals = self._extract_input_values(["c", "pe", "__kfid"], r1.text)
+        self.key = vals["__kfid"]
+
         body = {
             "__kfid": vals["__kfid"],
             "c": vals["c"],
@@ -110,6 +126,59 @@ class WFMUClient(object):
         r2 = self.session.post(
             "https://wfmu.org/playlistcommentpost.php", params={"p": 1}, data=body
         )
+
+    def get_comments(self) -> dict:
+        """
+        comments = []
+        comment = {}
+        r = self.session.get(COMMENTS_XML_URL)
+        soup = BeautifulSoup(r.text, "lxml-xml")
+        for c in soup.find_all("comment"):
+            comment["author"] = c.author
+            comment["content"] = c.content.plaintext.extract()
+            comment["parent"] = {
+                "type": c.parent.type,
+                "id": c.parent.id,
+                "content": c.parent,
+            }
+            comments.append(comment)
+            print(comment)
+            comment = {}
+
+        return comments
+        """
+        pass
+
+    # favorites
+    def favorite_current(self):
+        """
+        Shorthand for favorite(self.show.playlist_id, self.song.song_id)
+        """
+        self.favorite(self.show["playlist_id"], self.song["song_id"])
+
+    def favorite(self, playlist_id: str, song_id: str) -> None:
+        self._favcon_toggle(playlist_id, song_id, state=0)
+
+    def unfavorite(self, playlist_id: str, song_id: str) -> None:
+        self._favcon_toggle(playlist_id, song_id, state=1)
+
+    def _favcon_toggle(self, playlist_id: str, song_id: str, state) -> None:
+        url = "https://www.wfmu.org/favcon.php?action=fav_icon_toggle"
+        body = {
+            "type": "song",
+            "id": song_id,
+            "state": state,
+            "key": self.key,
+            "myurl": f"http://wfmu.org/playlists/shows/{playlist_id}",
+            "page_type": "playlist",
+            "page_id": playlist_id,
+        }
+        r = self.session.post(url, data=body)
+        pass
+
+    def get_favorites(self) -> dict:
+        # https://wfmu.org/auth.php?a=update_profile&panel_id=favorites
+        pass
 
     def _extract_input_values(self, names: list, html: str) -> dict:
         """
